c4c409e93ff7c08bcead406daa05032c5e5066da
Matteo Nastasi <nastasi@openquake.org>
2014-10-07 19:01:54 +0200

    received pylint annotations

--- pyline.py
+++ pyline.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
+"""
+Join lines of the same python statement.
+"""
 import sys
 
 class Pyline:
-    ST_BEGIN=0
-    ST_TRIPLE=1
-    ST_QUOTE=2
-    ST_COMMENT=3
+    """
+    Class to join lines of the same python statement.
+    """
+    ST_BEGIN = 0
+    ST_TRIPLE = 1
+    ST_QUOTE = 2
+    ST_COMMENT = 3
 
     def __init__(self, f_in, name_in):
         self.f_in    = f_in
@@ -13,21 +19,30 @@ class Pyline:
         self.data = f_in.read()
 
     @staticmethod
-    def backslash_newline(c):
-        if (c == '\n'):
+    def backslash_newline(cha):
+        """
+        Manage the character after a backslash.
+        """
+        if (cha == '\n'):
             return ''
         else:
-            return c
+            return cha
 
     @staticmethod
-    def joinlines(i, s, ct):
-        while i+1 < ct and (s[i+1] == ' ' or s[i+1] == '\t'):
+    def joinlines(i, s_in, s_len):
+        """
+        Remove all indentation spaces from joined line.
+        """
+        while i+1 < s_len and (s_in[i+1] == ' ' or s_in[i+1] == '\t'):
             i += 1
         return i
 
     def linearize(self):
-        s = self.data
-        ct = len(s)
+        """
+        Main method to join multi-lines statement.
+        """
+        s_in = self.data
+        s_len = len(s_in)
         c_pos = 0
         i = 0
         p_pare = 0
@@ -37,109 +52,111 @@ class Pyline:
 
         out = ""
 
-        st = self.ST_BEGIN
-        while i < ct:
+        status = self.ST_BEGIN
+        while i < s_len:
             if c_pos == 0:
                 out += ("" if self.name_in == "" else "%s:" % self.name_in)
                 c_pos = 1
-            # print "ST: %d S: [%s] I: %d  CT %d" % (st, s[i], i, ct)
-            if st == self.ST_BEGIN:
-                if (i + 2) < ct:
-                    if ((s[i] == '"' and s[i+1] == '"' and s[i+2] == '"') or
-                        (s[i] == "'" and s[i+1] == "'" and s[i+2] == "'")):
-                        st = self.ST_TRIPLE
-                        what_quote = s[i]
-                        out += s[i] *3
+            #print "ST: %d S: [%s] I: %d  S_LEN %d" % (status,
+            #                                          s_in[i], i, s_len)
+            if status == self.ST_BEGIN:
+                if (i + 2) < s_len:
+                    if s_in[i:i+3] == '"""' or s_in[i:i+3] == "'''":
+                        status = self.ST_TRIPLE
+                        what_quote = s_in[i]
+                        out += s_in[i] *3
                         i += 3
                         continue
-                elif (i + 1) < ct:
-                    if s[i] == '\\':
-                        out += self.backslash_newline(s[i+1])
+                elif (i + 1) < s_len:
+                    if s_in[i] == '\\':
+                        out += self.backslash_newline(s_in[i+1])
                         i += 2
                         continue
-                if s[i] == '#':
-                    st = self.ST_COMMENT
-                    out += s[i]
+                if s_in[i] == '#':
+                    status = self.ST_COMMENT
+                    out += s_in[i]
 
-                elif s[i] == '"' or s[i] == "'":
-                    what_quote = s[i]
-                    out += s[i]
-                    st = self.ST_QUOTE
+                elif s_in[i] == '"' or s_in[i] == "'":
+                    what_quote = s_in[i]
+                    out += s_in[i]
+                    status = self.ST_QUOTE
 
-                elif s[i] == '(':
-                    out += s[i]
+                elif s_in[i] == '(':
+                    out += s_in[i]
                     p_pare += 1
-                elif s[i] == ')':
-                    out += s[i]
+                elif s_in[i] == ')':
+                    out += s_in[i]
                     p_pare -= 1
 
-                elif s[i] == '[':
-                    out += s[i]
+                elif s_in[i] == '[':
+                    out += s_in[i]
                     p_brac += 1
-                elif s[i] == ']':
-                    out += s[i]
+                elif s_in[i] == ']':
+                    out += s_in[i]
                     p_brac -= 1
 
-                elif s[i] == '{':
-                    out += s[i]
+                elif s_in[i] == '{':
+                    out += s_in[i]
                     p_curl += 1
-                elif s[i] == '}':
-                    out += s[i]
+                elif s_in[i] == '}':
+                    out += s_in[i]
                     p_curl -= 1
 
-                elif s[i] == '\n':
+                elif s_in[i] == '\n':
                     if p_pare == 0 and p_brac == 0 and p_curl == 0:
-                        out += s[i]
+                        out += s_in[i]
                         c_pos = 0
                     else:
                         out += ' '
                         # remove next line exceeding spaces
-                        i = self.joinlines(i, s, ct)
+                        i = self.joinlines(i, s_in, s_len)
                 else:
-                    out += s[i]
-            elif st == self.ST_TRIPLE:
-                if (i + 2) < ct:
-                    if s[i] == what_quote and s[i+1] == what_quote and s[i+2] == what_quote:
-                        st = self.ST_BEGIN
-                        out += s[i] *3
+                    out += s_in[i]
+            elif status == self.ST_TRIPLE:
+                if (i + 2) < s_len:
+                    if (s_in[i] == what_quote and s_in[i+1] == what_quote and
+                        s_in[i+2] == what_quote):
+                        status = self.ST_BEGIN
+                        out += s_in[i] *3
                         i += 3
                         continue
-                elif (i + 1) < ct:
-                    if s[i] == '\\':
-                        out += self.backslash_newline(s[i+1])
+                elif (i + 1) < s_len:
+                    if s_in[i] == '\\':
+                        out += self.backslash_newline(s_in[i+1])
                         i += 2
                         continue
-                if s[i] == '\n':
+                if s_in[i] == '\n':
                     out += ' '
                     # remove next line exceeding spaces
-                    i = self.joinlines(i, s, ct)
+                    i = self.joinlines(i, s_in, s_len)
                 else:
-                    out += s[i]
-            elif st == self.ST_QUOTE:
-                if (i + 1) < ct:
-                    if s[i] == '\\':
-                        out += self.backslash_newline(s[i+1])
+                    out += s_in[i]
+            elif status == self.ST_QUOTE:
+                if (i + 1) < s_len:
+                    if s_in[i] == '\\':
+                        out += self.backslash_newline(s_in[i+1])
                         i += 2
                         continue
-                if s[i] == what_quote:
-                    out += s[i]
-                    st = self.ST_BEGIN
-                elif s[i] == '\n':
+                if s_in[i] == what_quote:
+                    out += s_in[i]
+                    status = self.ST_BEGIN
+                elif s_in[i] == '\n':
                     out += ' '
                     # remove next line exceeding spaces
-                    i = self.joinlines(i, s, ct)
+                    i = self.joinlines(i, s_in, s_len)
                 else:
-                    out += s[i]
-            elif st == self.ST_COMMENT:
-                if s[i] == '\n':
-                    st = self.ST_BEGIN
+                    out += s_in[i]
+            elif status == self.ST_COMMENT:
+                if s_in[i] == '\n':
+                    status = self.ST_BEGIN
                     c_pos = 0
-                out += s[i]
+                out += s_in[i]
 
-            # end of while i < ct ...
+            # end of while i < s_len ...
             i += 1
         if out[-1] != '\n':
-            sys.stderr.write("Warning: no newline at end of file %s\n" % self.name_in)
+            sys.stderr.write("Warning: no newline at end of file %s\n"
+                             % self.name_in)
             out += '\n'
         sys.stdout.write(out)
 
@@ -148,21 +165,28 @@ class Pyline:
 #
 
 def print_help(name):
+    """
+    Print command usage.
+    """
     print "USAGE\n  %s -h\n  %s [file1 [file2 [...]]]" % (name, name)
     sys.exit(0)
+
 def main():
+    """
+    Process files or stdin.
+    """
     if len(sys.argv) > 1 and (sys.argv[1] == '-h' or sys.argv[1] == '--help'):
         print_help(sys.argv[0])
         sys.exit(0)
 
     if len(sys.argv) == 1:
-        a = Pyline(sys.stdin, '')
-        a.linearize()
+        lin = Pyline(sys.stdin, '')
+        lin.linearize()
     else:
         for f_name in sys.argv[1:]:
             with open(f_name, "r") as f_in:
-                a = Pyline(f_in, f_name)
-                a.linearize()
+                lin = Pyline(f_in, f_name)
+                lin.linearize()
 
 if __name__ == '__main__':
     main()
