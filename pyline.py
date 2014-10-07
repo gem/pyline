9076a071ce001646e3bd4e59ccf020c710b8b119
Matteo Nastasi <nastasi@openquake.org>
2014-10-07 16:43:58 +0200

    functionalized part of the code, add usage, add multiple arguments management (no args => use stdin as input)

--- pyline.py
+++ pyline.py
@@ -7,15 +7,28 @@ class Pyline:
     ST_QUOTE=2
     ST_COMMENT=3
 
-    def __init__(self, name_in, name_out):
+    def __init__(self, f_in, name_in):
+        self.f_in    = f_in
         self.name_in = name_in
-        self.name_out = name_out
-        with open(self.name_in, "r") as f:
-            self.data = f.read()
+        self.data = f_in.read()
+
+    @staticmethod
+    def backslash_newline(c):
+        if (c == '\n'):
+            return ''
+        else:
+            return c
+
+    @staticmethod
+    def joinlines(i, s, ct):
+        while i+1 < ct and (s[i+1] == ' ' or s[i+1] == '\t'):
+            i += 1
+        return i
 
     def linearize(self):
         s = self.data
         ct = len(s)
+        c_pos = 0
         i = 0
         p_pare = 0
         p_brac = 0
@@ -26,6 +39,9 @@ class Pyline:
 
         st = self.ST_BEGIN
         while i < ct:
+            if c_pos == 0:
+                out += ("" if self.name_in == "" else "%s:" % self.name_in)
+                c_pos = 1
             # print "ST: %d S: [%s] I: %d  CT %d" % (st, s[i], i, ct)
             if st == self.ST_BEGIN:
                 if (i + 2) < ct:
@@ -38,7 +54,7 @@ class Pyline:
                         continue
                 elif (i + 1) < ct:
                     if s[i] == '\\':
-                        out += s[i+1]
+                        out += self.backslash_newline(s[i+1])
                         i += 2
                         continue
                 if s[i] == '#':
@@ -74,11 +90,11 @@ class Pyline:
                 elif s[i] == '\n':
                     if p_pare == 0 and p_brac == 0 and p_curl == 0:
                         out += s[i]
+                        c_pos = 0
                     else:
                         out += ' '
                         # remove next line exceeding spaces
-                        while i+1 < ct and (s[i+1] == ' ' or s[i+1] == '\t'):
-                            i += 1
+                        i = self.joinlines(i, s, ct)
                 else:
                     out += s[i]
             elif st == self.ST_TRIPLE:
@@ -90,20 +106,19 @@ class Pyline:
                         continue
                 elif (i + 1) < ct:
                     if s[i] == '\\':
-                        out += s[i+1]
+                        out += self.backslash_newline(s[i+1])
                         i += 2
                         continue
                 if s[i] == '\n':
                     out += ' '
                     # remove next line exceeding spaces
-                    while i+1 < ct and (s[i+1] == ' ' or s[i+1] == '\t'):
-                        i += 1
+                    i = self.joinlines(i, s, ct)
                 else:
                     out += s[i]
             elif st == self.ST_QUOTE:
                 if (i + 1) < ct:
                     if s[i] == '\\':
-                        out += s[i+1]
+                        out += self.backslash_newline(s[i+1])
                         i += 2
                         continue
                 if s[i] == what_quote:
@@ -112,29 +127,45 @@ class Pyline:
                 elif s[i] == '\n':
                     out += ' '
                     # remove next line exceeding spaces
-                    while i+1 < ct and (s[i+1] == ' ' or s[i+1] == '\t'):
-                        i += 1
+                    i = self.joinlines(i, s, ct)
                 else:
                     out += s[i]
             elif st == self.ST_COMMENT:
                 if s[i] == '\n':
                     st = self.ST_BEGIN
+                    c_pos = 0
                 out += s[i]
-                
+
             # end of while i < ct ...
             i += 1
-
-        print out
+        if out[-1] != '\n':
+            sys.stderr.write("Warning: no newline at end of file %s\n" % self.name_in)
+            out += '\n'
+        sys.stdout.write(out)
 
 #
 #  MAIN
 #
 
-if len(sys.argv) != 2:
-    sys.exit(12)
+def print_help(name):
+    print "USAGE\n  %s -h\n  %s [file1 [file2 [...]]]" % (name, name)
+    sys.exit(0)
+def main():
+    if len(sys.argv) > 1 and (sys.argv[1] == '-h' or sys.argv[1] == '--help'):
+        print_help(sys.argv[0])
+        sys.exit(0)
 
-name_in = sys.argv[1]
-name_out = "linearized.py"
+    if len(sys.argv) == 1:
+        a = Pyline(sys.stdin, '')
+        a.linearize()
+    else:
+        for f_name in sys.argv[1:]:
+            with open(f_name, "r") as f_in:
+                a = Pyline(f_in, f_name)
+                a.linearize()
 
-a = Pyline(name_in, name_out)
-a.linearize()
+if __name__ == '__main__':
+    main()
+
+
+
