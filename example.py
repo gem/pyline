4ce4e20823ab673f6188980d9f20cacb58957d66
Matteo Nastasi <nastasi@openquake.org>
2014-10-07 16:41:44 +0200

    add comments with opened parenthesis and no new line at the end of the file in the example file

--- example.py
+++ example.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python ((
 
 if __name__ == "__main__":
     print "whe are here %s %s %s %s" % ("uno",
-                                         'due',
+                                         'due
+ due bis',
                                          "tre",
                                          "qua")
 print "fine"
+# zavorra
